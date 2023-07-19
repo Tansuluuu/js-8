b3c87ed5f81b7787de0daadc631215277790d33a
Tansuluuu <tynalievatan@gmail.com>
2023-07-19 21:53:43 +0600

    made home work 8

---
+++ script.js
@@ -0,0 +1,96 @@
+
+// Home work
+// 1) Дан массив с элементами [1, 2, 3, 4, 5].
+// С помощью цикла for найдите сумму элементов этого массива. Запишите ее в переменную result.
+
+// const array = [1,2,3,4,5]
+//
+// const yellow = (arg) => {
+//   let sum = 0;
+//   for (let index = 0; index < arg.length; index++){
+//       sum += arg[index]
+//   }
+//   return sum;
+// }
+// console.log(yellow(array))
+
+// 2
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//
+// const yellow = (arg) => {
+//     let sum = 0;
+//     for (let index = 0; index < arg.length; index++){
+//         sum += arg[index]
+//     }
+//     return sum;
+// }
+// console.log(yellow(array))
+
+//3
+// 3) Дан массив с элементами [1, 2, 5, 9, 4, 13, 4, 10].
+// С помощью цикла for и оператора if проверьте есть ли в массиве элемент со значением,
+// равным 4. Если есть - выведите на экран 'Есть!' и выйдите из цикла. Если нет - ничего делать не надо.
+
+
+// const yellow = (arr) => {
+//     for(i = 0; i < arr.length; i++){
+//         if(arr[i] === 4) {
+//             return 'have'
+//         }
+//     }
+// }
+// console.log(yellow([1, 2, 5, 9, 4, 13, 4, 10]))
+
+
+
+
+
+
+// practice
+
+
+
+// const yellow = (cars) => {
+//     delete cars.color
+//   return cars.map(car => car.year === 2019 ? {...car, price: car.price -5000 , } : car)
+// }
+// берет старый обьект и меняет его в новом месте
+
+
+// const array = [1,2,3,4,5,6,7]
+//
+// const yellow = (arg) => {
+//   let sum = 0;
+//   for (let index = 0; index < arg.length; index++){
+//       sum += arg[index]
+//   }
+//   return sum;
+// }
+// yellow(array)
+// console.log(yellow(array))
+
+
+// 1
+// const array = [1,2,3,4,5,6,7,8,9,10]
+// const yellow = (array) => {
+//   let sum = 0;
+//   for ( let index = 0; index < array.length; index++){
+//       sum += array[index]
+//   }
+//   return sum
+// }
+// console.log(yellow(array))
+
+
+//2
+// const yellow = (arg) => {
+//   return arg.split('').reduce((acc,el,idx) => {
+//       if (idx % 2 === 0){
+//           acc.push(el)
+//       }
+//       return acc
+//   }, [])
+// }
+// console.log(yellow('lorem ipsum dolor sit amet'))
+
+
